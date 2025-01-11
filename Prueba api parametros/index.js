991e6ba2f1904b2e0da5c9b01ba5d537a31e59d8
-vv <lacran119@gmail.com>
2025-01-10 18:04:04 -0600

    Actualizacion de documentos

--- Prueba api parametros/index.js
+++ Prueba api parametros/index.js
@@ -1,31 +1,32 @@
-let variable = "name";
 const contenedor = document.getElementById('contenedor');
 const opciones = document.getElementById('options');
 
+/* En un ciclo se introducen las etiquetas option al interior 
+de la etiqueta  select en el HTML con un autoincrmento para numerarlos
+ */
 for (let index = 0; index <= 44; index++) {
     opciones.innerHTML += `echo("<option value='${index}'>${index}</option>");`;
 
 }
-
+//Se RecuperaEl valor del select de HTML y se crea una funcion
 document.getElementById("options").addEventListener("change", function(){
     
 
+    /*
+    Se recupera la API y se le asigna a data
+    Al final de la url se le agrega el valor escogido en el select de HTML
+    para recuperar los datos de esa instancia especifica
+    */
     fetch(`https://dragonball-api.com/api/transformations/${this.value}`)
     .then(response => response.json())
     .then(data => {
 
-        /*
-        imagen  = JSON.stringify(data.image).replaceAll('"','');
-        nombre  = JSON.stringify(data.name).replaceAll('"','');
-        power   = JSON.stringify(data.ki).replaceAll('"','');
 
-        <h1>${data.character.name.replaceAll('"','')}</h1>
+        /* console.log(data.id);
+        console.log(data.image); */
 
-        */
-        //alert("you selected: " +  this.data.name);
-        //alert(data.character.name);
-        console.log(data.id);
-        console.log(data.image);
+        /* Se introduce el siguiente fragmento HTML al 
+        interior del div contenedor */
         contenedor.innerHTML =
         `
             <div class="card">
